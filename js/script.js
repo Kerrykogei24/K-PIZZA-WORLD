32acd5716d2ecba851c8030e50cf0a0a456a9940
Kerrykogei24 <kerrykomar@gmail.com>
2020-10-17 00:39:10 +0300

    add functionality to the form

--- js/script.js
+++ js/script.js
@@ -15,3 +15,33 @@ $(document).ready(function() {
         $("#delivery").toggle();
     });
 });
+
+function orderNow() {
+
+    var flavor = $("input#pizza-type").val();
+    var crust = $("input#size").val();
+    var size = $("input#crust").val();
+    var number = $("input#number").val();
+
+    var ordePrice;
+    if (flavor === "pizza1" || flavor === "pizza2" || flavor === "pizza3") {
+        if (size === "small") {
+            ordePrice === 700;
+        } else if (size === "medium") {
+            orderPrice === 1000;
+        } else if (size === "large") {
+            orderPrice === 1400;
+        }
+    } else if (flavor === " pizza4" || flavor === "pizza5" || flavor === "pizza6") {
+        if (size === "small") {
+            ordePrice === 800;
+        } else if (size === "medium") {
+            orderPrice === 1100;
+        } else if (size === "large") {
+            orderPrice === 1600;
+        }
+
+    } else {
+        alert("You  have not made any order")
+    }
+}
