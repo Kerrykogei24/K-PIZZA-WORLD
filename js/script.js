6a48d01cbe17dfe0cd3a64f337dcda6294ec2fe7
Kerrykogei24 <kerrykomar@gmail.com>
2020-10-17 16:09:33 +0300

    define prices

--- js/script.js
+++ js/script.js
@@ -1,47 +1,74 @@
-$(document).ready(function() {
-    $("#firstselect").click(function() {
-        $("#checkoutdetails").toggle();
-    })
-    $("#secondselect").click(function() {
-        $("#deliveryoption").toggle();
-    })
-    $("#pickup").click(function() {
-        $("#pick").toggle();
-    })
-    $("#letsdeliver").click(function() {
-        $("#pickup2").toggle();
-    })
-    $("#thirdsubmit").click(function() {
-        $("#delivery").toggle();
-    });
-});
-
 function orderNow() {
 
-    var flavor = $("input#pizza-type").val();
-    var crust = $("input#size").val();
-    var size = $("input#crust").val();
-    var number = $("input#number").val();
+    // var flavor = document.getElementById("pizzatype").value;
 
-    var ordePrice;
-    if (flavor === "pizza1" || flavor === "pizza2" || flavor === "pizza3") {
+    // var crust = document.getElementById("crust").value;
+
+    // var size = document.getElementById("size").value;
+
+    // var number = document.getElementById("number").value;
+    var name = $("input#name").val();
+    var flavor = $("#pizzatype").val();
+    var crust = $("#size").val();
+    var size = $("#crust").val();
+    var number = $("#pizza-no").val();
+    var topping = [];
+    $.each($('input[name="toppings"]:checked'),
+        function() {
+            topping.push($(this).val());
+        }
+    );
+
+    var orderPrice;
+    if (flavor === "Margherita Pizza" || flavor === "Pepperoni Pizza" || flavor === "BBQ Chicken Pizza") {
         if (size === "small") {
-            ordePrice === 700;
+            orderPrice === 700;
         } else if (size === "medium") {
             orderPrice === 1000;
         } else if (size === "large") {
             orderPrice === 1400;
         }
-    } else if (flavor === " pizza4" || flavor === "pizza5" || flavor === "pizza6") {
+    } else if (flavor === "BBQ Chicken Pizza" || flavor === "Hawaiian Pizza" || flavor === "Meat-Lover's Pizza") {
         if (size === "small") {
-            ordePrice === 800;
+            orderPrice === 800;
         } else if (size === "medium") {
             orderPrice === 1100;
         } else if (size === "large") {
             orderPrice === 1600;
         }
 
-    } else {
-        alert("You  have not made any order")
     }
+
+    var crustPrice;
+
+    if (crust === "crispy") {
+        crustPrice === 200;
+    } else if (crust === "stuffed") {
+        crustPrice === 170;
+    } else if (crust === "gluten-free") {
+        crustPrice === 150;
+    }
+
+    var selected = $('input[name="toppings"]:checked') var toppingPrice;
+    if ()
+
 }
+
+
+// $(document).ready(function() {
+//     $("#firstselect").click(function() {
+//         $("#checkoutdetails").toggle();
+//     })
+//     $("#secondselect").click(function() {
+//         $("#deliveryoption").toggle();
+//     })
+//     $("#pickup").click(function() {
+//         $("#pick").toggle();
+//     })
+//     $("#letsdeliver").click(function() {
+//         $("#pickup2").toggle();
+//     })
+//     $("#thirdsubmit").click(function() {
+//         $("#delivery").toggle();
+//     });
+// });
