dd0d74a96e687fa1d1175b3ac0c3aca22fc81fb2
Kerrykogei24 <kerrykomar@gmail.com>
2020-10-18 01:39:46 +0300

    modified js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function orderNow() {
     var size = $("#crust").val();
     var number = $("#pizza-no").val();
     var topping = [];
-    $.each($('input[name="toppings"]:checked'),
+    $.each($('input[name="topping"]:checked'),
         function() {
             topping.push($(this).val());
         }
@@ -41,20 +41,103 @@ function orderNow() {
 
     var crustPrice;
 
-    if (crust === "crispy") {
+    if (crust === "Crispy") {
         crustPrice === 200;
-    } else if (crust === "stuffed") {
+    } else if (crust === "Stuffed") {
         crustPrice === 170;
-    } else if (crust === "gluten-free") {
+    } else if (crust === "Gluten") {
         crustPrice === 150;
     }
 
-    var selected = $('input[name="toppings"]:checked') var toppingPrice;
-    if ()
+    var selected = $('input[name="topping"]:checked').length;
+    var toppingPrice;
+    if (size === "large") {
+        toppingPrice === selected * 200;
+    } else if (size === "medium") {
+        toppingPrice === selected * 150;
+    } else if (size === "small") {
+        toppingPrice === selected * 100;
+    }
+
+
+    $("input[type='checkbox']:not(:checked)").prop({
+        disabled: true
+    });
+    $('#thirdsubmit').prop('disabled', true);
+    $("#checkoutdetails").show();
+
+    var price = (orderPrice + crustPrice + toppingPrice);
+    var totalPrice = parseInt(price * number);
+
+    $(".clientname").text(name);
+    // $(".clientname").append('<tr><td id="clientname">' + name);
+    $(".number").append('<tr><td id="pizza-no">' + number);
+    $(".size").append('<tr><td id="size">' + size);
+    $(".crust").append('<tr><td id="crust">' + crust);
+    $(".flavor").append('<tr><td id="pizzatype">' + flavor);
+    $(".total").append('<tr><td id="total">' + totalPrice);
+
+
+
+
+
+    arrayTotal.push(totalPrice);
+    if (topping == "") {
+        $(".topping").append('<tr><td id="ptopping">' + "-");
+    }
+    if (topping != "") {
+        $(".topping").append('<tr><td id="ptopping">' + topping);
+
+        $(".name").text(name);
+    } else {
+        alert("pick atleast 2 toppings");
+    }
+
+
+
+
+}
+$(document).ready(function() {
+    $("#order").submit(function(event) {
+        event.preventDefault();
+        orderNow();
+    });
+    $("#checkoutdetails").submit(function(event) {
+        event.preventDefault();
+        letsdeliver();
+    });
+});
+
+function letsDeliver() {
+    $("#pickup2 ").show();
+
+    var location = $("input#location").val();
+    var phone = $("input#phone").val();
+
+    $(".location").text(location);
+    $(".phone").text(phone);
+    $("#delivery").hide();
+
 
 }
 
+function checkOut() {
+    $("#deliveryoption").show();
+    $("#checkoutdetails").hide();
+}
 
+function deliverNow() {
+    $("#pickup2").hide();
+    $("#delivery").show();
+}
+$(document).ready(function() {
+    $("#thirdsubmit").submit(function(event) {
+        event.preventDefault();
+        deliverNow();
+
+    });
+});
+// function letsDeliver()
 // $(document).ready(function() {
 //     $("#firstselect").click(function() {
 //         $("#checkoutdetails").toggle();
@@ -71,4 +154,4 @@ function orderNow() {
 //     $("#thirdsubmit").click(function() {
 //         $("#delivery").toggle();
 //     });
-// });
+// })
